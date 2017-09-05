d0d937ac7a7a6367751521dbbfcd34d0d7643077
João Pedro Vilas Boas <joaopedrovbs@gmail.com>
2017-09-05 09:43:00 -0300

    Change Server Port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const bot = require('./bot')
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 3000
+
 app.post('/api', function (req, res) {
   res.send('Hello World!')
   bot.run()
@@ -12,6 +14,6 @@ app.get('/favicon.ico', function(req, res) {
     res.status(404);
 });
 
-app.listen(80, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 80!')
 })
